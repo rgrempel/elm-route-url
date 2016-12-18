c1bce645210f6c76c64b6ae8e40d20d8c33e84ff
Roman Frołow <rofrol@gmail.com>
2016-12-18 20:48:52 +0100

    use absolute path for gif

--- examples/elm-architecture-tutorial/Example5/RandomGif.elm
+++ examples/elm-architecture-tutorial/Example5/RandomGif.elm
@@ -38,7 +38,7 @@ initial request we issue here.
 -}
 init : ( Model, Cmd Action )
 init =
-    ( Model "funny cats" "assets/waiting.gif" Use
+    ( Model "funny cats" "/assets/waiting.gif" Use
     , getRandomGif "funny cats"
     )
 
